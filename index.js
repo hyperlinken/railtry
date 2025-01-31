ec3ac1d4a755bba5f3a0ff3c8b75abedeb9684e2
harsh <hs26042004markam@gmail.com>
2025-02-01 01:35:33 +0530

    socket http axios cors added pkglockjson modified

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require("express")
-const app= express();
+const http=require('http');
+const socketIo=require('socket.io');
+const axios = require('axios');
+const cors = require('cors');
 
-app.get("/" , (req , res)=> {
+const app= express();
+const server=http.createServer(app);
+
+app.use(cors({
+    origin: 'https://hyperlinken.github.io', // Allow your frontend origin
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+
+const io = new Server(server, {
+    cors: {
+        origin: 'https://hyperlinken.github.io', // Allow WebSocket connections from this origin
+        methods: ['GET', 'POST'],
+    },
+});
+
+app.get("/hi" , (req , res)=> {
     res.send("hi");
 })
 
-app.listen( 5000 , () =>{
-    console.log("server running");
+io.on('connection',(socket)=>{
+    console.log('new connection has been established\n');
+    socket.on('user-msg',(message)=>{
+        io.emit('msg',message);
+    })
+})
+
+const port=process.env.PORT || 5000;
+app.listen( port , () =>{
+    console.log("server is running");
 })
