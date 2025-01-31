0ee6c4fd78e09a015073319a9f503a12c4eb3a34
harsh <hs26042004markam@gmail.com>
2025-02-01 01:46:39 +0530

    modified

--- index.js
+++ index.js
@@ -5,20 +5,12 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app= express();
-const server=http.createServer(app);
 
 app.use(cors({
     origin: 'https://hyperlinken.github.io', // Allow your frontend origin
     methods: ['GET', 'POST'],
     credentials: true,
 }));
-
-const io = new Server(server, {
-    cors: {
-        origin: 'https://hyperlinken.github.io', // Allow WebSocket connections from this origin
-        methods: ['GET', 'POST'],
-    },
-});
 
 app.get("/hi" , (req , res)=> {
     res.send("hi");
