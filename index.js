8786af96526c161a0cc8e4b0ae9c92c2c3b7f8e1
harsh <hs26042004markam@gmail.com>
2025-02-01 02:02:45 +0530

    modified

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express")
+const http=require('http');
 const socketIo=require('socket.io');
 const cors = require('cors');
 
 const app= express();
+const server = http.createServer(app);
 
-const io = socketIo(server, { // Initialize Socket.IO with the server
+app.use(cors({
+    origin: 'https://hyperlinken.github.io', // Allow your frontend origin
+    methods: ['GET', 'POST'],
+    credentials: true,
+}));
+
+const io = socketIo(server, {
     cors: {
         origin: 'https://hyperlinken.github.io',
         methods: ['GET', 'POST'],
